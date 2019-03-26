99a8e0fd0c72f49aac9b22ddffb8a42b1f83665e
oscaredu22 <oscaredu03@hotmail.com>
2019-03-26 11:39:15 -0500

    validacion parte 1

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+declare var jquery: any;
+declare var $: any;
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
   }
 
+  btnClickHome = function () {
+    var validacionUser = $('#user').val();
+    var validacionPassword = $('#password').val();
+
+    if (validacionUser === "" || validacionPassword === "") {
+      alert('Debes llenar todos los campos');
+      return false;
+    }
+    else {
+      this.router.navigateByUrl('/inicio');
+    }
+  };
 }
